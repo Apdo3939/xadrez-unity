a57104ddd8c250171d219e612e909848555c8f96
Apdo3939 <apdo3939@hotmail.com>
2021-09-20 19:41:42 -0300

    Add model for pieces and controller

---
+++ Assets/Scripts/Piece.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Piece : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        Debug.Log("Clik na piece " + transform);
+    }
+}
