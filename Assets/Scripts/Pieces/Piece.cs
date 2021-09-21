785eb2f19690f083ee22e9bc219e86b58ae09f77
Apdo3939 <apdo3939@hotmail.com>
2021-09-21 16:31:37 -0300

    Add board logic

--- Assets/Scripts/Pieces/Piece.cs
+++ Assets/Scripts/Pieces/Piece.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public abstract class Piece : MonoBehaviour
 {
+    public Tile tile = new Tile();
     void OnMouseDown()
     {
         Debug.Log("Clik na piece " + transform);
+    }
+
+    void Start()
+    {
+        Board.instance.AddPiece(transform.parent.name, this);
     }
 }
